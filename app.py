af59da2e717e7bce8be16bd79ccbbe8969fcdc56
Yipvyip <yyhjokingonlyla@gmail.com>
2024-05-25 14:52:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def main():
     if flag == 1:
         name = request.form.get("q")
         current_time = datetime.datetime.now()
-        conn = sqlite3.connect("log (1).db")
+        conn = sqlite3.connect("log.db")
         c = conn.cursor()
         c.execute("insert into user (name,time) values (?,?)",(name,current_time))
         conn.commit()
@@ -68,7 +68,7 @@ def image_result():
 
 @app.route("/log",methods=["GET","POST"])
 def log():
-    conn = sqlite3.connect("log (1).db")
+    conn = sqlite3.connect("log.db")
     c = conn.cursor()
     c.execute("select * from user")
     r = ""
@@ -82,7 +82,7 @@ def log():
 
 @app.route("/delete",methods=["GET","POST"])
 def delete():
-    conn = sqlite3.connect('log (1).db')
+    conn = sqlite3.connect('log.db')
     c = conn.cursor()
     c.execute("delete from user")
     conn.commit()
